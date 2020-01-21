8edb6db21da257617657539346b9370b61d92fee
97Vix <56716721+97Vix@users.noreply.github.com>
2020-01-21 17:12:20 +0100

    Add files via upload

--- save/move.py
+++ save/move.py
@@ -1,7 +1,7 @@
 import time,datetime
 import RPi.GPIO as GPIO
 import mysql.connector
- 
+import os
  
  
 mydb = mysql.connector.connect(
@@ -20,77 +20,116 @@ GPIO.setup(GPIO_PIR,GPIO.IN)
 Current_State  = 0
 Previous_State = 0
 try:
-
+ print("ready")
 
  while GPIO.input(GPIO_PIR)==1:
 
    Current_State  = 0
    
-   print ("  Ready")
- #stop_time=time.time()
 
-
- while True :
-
-   time.sleep(0.005)
-   Current_State = GPIO.input(GPIO_PIR)
-
-   if Current_State==1 and Previous_State==0:
-
-     #start_time=time.time()
-    # print (datetime.datetime.now(),)
-
-     mycursor = mydb.cursor()
-
-     sql = "INSERT INTO move (time) VALUES (%s)"
-     val = (datetime.datetime.now(),)
-     mycursor.execute(sql, val)
-
-     mydb.commit()
-
-     print(mycursor.rowcount, "record inserted.")
+ 
+ while 1:
      
+   time.sleep(0.5)
+   
+   mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      passwd="callheni",
+      database="DB_CPU"
+   )
+     
+   mycursor = mydb.cursor()
+   mycursor.execute("SELECT movesensor FROM test2 WHERE ID=12 ")
+
+   myresult = mycursor.fetchall()
+
+   y=str(myresult)
+   y=(y.strip('('','')''['']'))
+   y=int(y)
+   print ("lekerdez",y)
+   move=y
+   
+   
+   
+   if move==1:
+    #time.sleep(0.005)
+    Current_State = GPIO.input(GPIO_PIR)
+
+    if Current_State==1 and Previous_State==0:
 
 
+      mycursor = mydb.cursor()
 
-    # print ("  Moving ",)
+      sql = "INSERT INTO move (time) VALUES (%s)"
+      val = (datetime.datetime.now(),)
+      mycursor.execute(sql, val)
+      mydb.commit()
 
-    # idle_time=int(start_time-stop_time)
+      print(mycursor.rowcount, "record inserted.")
+      print (y)
+      os.system("echo -e 'Subject: Riasztás\r\n\r\nRiasztás!' |msmtp --debug --from=default -t gvardian.levente@gmail.com")
+      while y!=0:
+       os.system("omxplayer 2.mp3 -o alsa")
+ 
+       mydb = mysql.connector.connect(
+       host="localhost",
+       user="root",
+       passwd="callheni",
+       database="DB_CPU"
+       )
+     
+       mycursor = mydb.cursor()
+       mycursor.execute("SELECT movesensor FROM test2 WHERE ID=12 ")
 
-    # print (" (Idle time : " + str(idle_time) + " secs)")
+       myresult = mycursor.fetchall()
 
-  
+       y=str(myresult)
+       y=(y.strip('('','')''['']'))
+       y=int(y)
+       print ("lekerdez1",y)
+       move=y
+       
+       
+      Previous_State=1
+    elif Current_State==0 and Previous_State==1:
 
-     Previous_State=1
-
-   elif Current_State==0 and Previous_State==1:
-
-
-
-    # stop_time=time.time()
-
-    #print (datetime.datetime.now(),)
      
      
-     mycursor = mydb.cursor()
+      mycursor = mydb.cursor()
 
-     sql = "INSERT INTO move (time) VALUES (%s)"
-     val = (datetime.datetime.now(),)
-     mycursor.execute(sql, val)
+      sql = "INSERT INTO move (time) VALUES (%s)"
+      val = (datetime.datetime.now(),)
+      mycursor.execute(sql, val)
 
-     mydb.commit()
+      mydb.commit()
 
-     print(mycursor.rowcount, "record inserted.")
+      print(mycursor.rowcount, "record inserted.")
+      os.system("echo -e 'Subject: Riasztás\r\n\r\nRiasztás!' |msmtp --debug --from=default -t gvardian.levente@gmail.com")
+      while y!=0:
+       os.system("omxplayer 2.mp3 -o alsa")
+
+       mydb = mysql.connector.connect(
+       host="localhost",
+       user="root",
+       passwd="callheni",
+       database="DB_CPU"
+       )
      
-     
+       mycursor = mydb.cursor()
+       mycursor.execute("SELECT movesensor FROM test2 WHERE ID=12 ")
 
-    # print ("  Standby  "),
+       myresult = mycursor.fetchall()
 
-    # elapsed_time=int(stop_time-start_time)
+       y=str(myresult)
+       y=(y.strip('('','')''['']'))
+       y=int(y)
+       print ("lekerdez2",y)
+       move=y
 
-    # print (" (Elapsed time : " + str(elapsed_time) + " secs)")
 
-     Previous_State=0
+
+      Previous_State=0
 
  
 
